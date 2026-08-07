d6a7f4ca1a72f7fa457e162fb610d1aead77b05f
Vic <boy012929@gmail.com>
2026-08-07 15:57:07 +0800

    add :booking,bookingrepository,bookingorder,bookingorderRepository

---
+++ src/main/java/com/hotel/entity/Booking.java
@@ -0,0 +1,48 @@
+package com.hotel.entity;
+
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "booking")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Booking {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "booking_id")
+	private Integer bookingId;
+
+	@Column(name = "booking_order_id")
+	private Integer bookingOrderId;
+
+	@Column(name = "check_in_date")
+	private Date checkInDate;
+
+	@Column(name = "check_in_date")
+	private Date checkOutDate;
+
+	@Column(name = "guest_num")
+	private Integer guestNum;
+
+	@Column(name = "booking_status")
+	private String bookingStatus;
+
+	@Column(name = "room_id")
+	private Integer roomId;
+
+	@Column(name = "room_type_id")
+	private Integer roomTypeId;
+
+}
