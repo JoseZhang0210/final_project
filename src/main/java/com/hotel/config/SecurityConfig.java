832691f307984861f8906685403ad2c218e3e8db
Vic Li <boy012929@mail.com>
2026-08-14 01:22:14 +0800

    add html

--- src/main/java/com/hotel/config/SecurityConfig.java
+++ src/main/java/com/hotel/config/SecurityConfig.java
@@ -14,22 +14,46 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
 
-	@Bean
-	SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-		return httpSecurity.csrf(csrf -> csrf.disable())
-				.formLogin(Customizer.withDefaults())
-				.authorizeHttpRequests(requests -> requests
-						// Get 白名單
-						.requestMatchers(HttpMethod.GET, "/", "/register", "/error").permitAll()
-						// POST 白名單
-						.requestMatchers(HttpMethod.POST, "/accounts").permitAll()
-						.anyRequest().authenticated())
-				.httpBasic(Customizer.withDefaults())
-				.build();
-	}
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity.csrf(csrf -> csrf.disable())
+                .formLogin(Customizer.withDefaults())
+                .authorizeHttpRequests(requests -> requests
+                // GET 白名單
+                .requestMatchers(HttpMethod.GET,
+                        "/",
+                        "/register",
+                        "/error",
+                        "/*.html",
+                        "/bookingorders/**",
+                        "/static/**",
+                        "/css/**",
+                        "/js/**"
+                ).permitAll()
+                // POST 白名單
+                .requestMatchers(HttpMethod.POST, "/accounts", "/bookingorders/**").permitAll()
+                // 其餘需要驗證
+                .anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults())
+                .build();
+    }
 
-	@Bean
-	PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
+    // @Bean
+    // SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+    //     return httpSecurity.csrf(csrf -> csrf.disable())
+    //             .formLogin(Customizer.withDefaults())
+    //             .authorizeHttpRequests(requests -> requests
+    //             // Get 白名單
+    //             .requestMatchers(HttpMethod.GET, "/", "/register", "/error").permitAll()
+    //             // POST 白名單
+    //             .requestMatchers(HttpMethod.POST, "/accounts", "/bookingorders/**").permitAll()
+    //             .anyRequest().authenticated())
+    //             .httpBasic(Customizer.withDefaults())
+    //             .build();
+    // }
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
