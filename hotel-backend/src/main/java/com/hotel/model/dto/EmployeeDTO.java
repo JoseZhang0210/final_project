784a0ce757c1da2982cb032bf6e2ee1901c76a31
Jose Asus <jose4775934@gmail.com>
2026-08-22 12:34:35 +0800

    feat/add employee restful api

---
+++ hotel-backend/src/main/java/com/hotel/model/dto/EmployeeDTO.java
@@ -0,0 +1,48 @@
+package com.hotel.model.dto;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class EmployeeDTO {
+
+    // Employee 表
+    private Integer employeeId;
+    private Integer departmentId;
+    private String departmentName;
+    private String position;
+
+    @JsonProperty("isAdmin")
+    private Boolean isAdmin;
+
+    // Account 表
+    private Integer accountId;
+    private String username;
+    private String password;
+    private String status;
+
+    // Profile 表
+    private Integer profileId;
+    private String name;
+    private String email;
+    private String phone;
+    private String zipcode;
+    private String city;
+    private String district;
+    private String address;
+    private LocalDateTime createdAt;
+    private LocalDate birthday;
+    private String gender;
+    private LocalDateTime updatedAt;
+}
+
